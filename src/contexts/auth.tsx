4bb3f33452aa79756171ee7b190044bb319f6681
Klayver Ximenes <klayverx@gmail.com>
2021-05-29 15:20:18 -0300

    adiciona axios e context API

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,30 +1,79 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
+import { useHistory } from "react-router";
+
 import api from '../services/api';
 interface Props {
     children?: React.ReactNode
 }
-
+interface User {
+    name: string
+}
 interface AuthContextData {
     signed: boolean,
-    setSigned: (signed: boolean) => void
+    signIn: () => void
+    loading: boolean
+    user: User
 }
 
 const initialState = {
     signed: false,
-    setSigned: () => { }
+    setSigned: () => { },
+    signIn: () => { },
+    loading: false,
+    token: null,
+    user: {
+        name: ''
+    }
 }
 
 const AuthContext = createContext<AuthContextData>(initialState)
 
 function AuthProvider({ children }: Props) {
+    const [user, setUser] = useState<User>(initialState.user)
+    const [loading, setLoading] = useState(initialState.loading)
     const [signed, setSigned] = useState(initialState.signed)
 
+    const history = useHistory()
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('@Auth:TOKEN')
+        const storedUser = localStorage.getItem('@Auth:USER')
+
+        if (storedToken && storedUser) {
+            setUser(JSON.parse(storedUser))
+            setSigned(true)
+
+            api.defaults.headers.authorization = `Bearer ${storedToken}`
+        }
+    }, [])
+
     async function signIn() {
-        await api.post('/signin')
+        try {
+            setLoading(true)
+
+            const { data } = await api.post('/signin')
+
+            const apiUser = {
+                name: data.name
+            }
+
+            api.defaults.headers.authorization = `Bearer ${data.token}`
+
+            localStorage.setItem('@Auth:TOKEN', data.token)
+            localStorage.setItem('@Auth:USER', JSON.stringify(apiUser))
+
+            setUser(apiUser)
+            setSigned(true)
+            setLoading(false)
+
+            history.push('/dashboard')
+        } catch (err) {
+
+        }
     }
 
     return (
-        <AuthContext.Provider value={{ signed, setSigned }}>
+        <AuthContext.Provider value={{ signed, signIn, loading, user }}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,15 +6,19 @@ import logo from '../../assets/logo.png';
 import { Container, Logo, Input, Button } from './styles'
 
 function SignIn() {
-    const { signed, setSigned } = useContext(AuthContext)
+    const { signIn, loading } = useContext(AuthContext)
+
+    function handleSignIn() {
+        signIn()
+    }
 
     return (
         <Container>
             <Logo src={logo} alt="CL Logo" />
             <Input type="text" defaultValue='teste@email.com' />
             <Input type="password" defaultValue='12345678' />
-            <Button onClick={() => setSigned(true)}>
-                Entrar
+            <Button onClick={handleSignIn}>
+                {loading ? 'Carregando' : 'Entrar'}
             </Button>
         </Container>
     );
